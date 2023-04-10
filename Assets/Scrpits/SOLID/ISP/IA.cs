bcdee1bdab028a63bf58eb769bfe7f3b5554b2b0
Rony <siamore9724@gmail.com>
2023-04-10 10:46:17 +0900

    plus examples

--- Assets/Scrpits/SOLID/ISP/IA.cs
+++ Assets/Scrpits/SOLID/ISP/IA.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IA : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+namespace ISP {
+    public interface IA {
+        public void A();
     }
 }
+
