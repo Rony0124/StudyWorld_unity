510ff8322c58b24ad22072452b6fd0f06f6411c9
rony <siamore9724@gmail.com>
2023-12-10 11:40:32 +0900

    SO Event System 1차 업로드

---
+++ Assets/Scrpits/SOEventSystem/EventHolder.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Scrpits.SOEventSystem
+{
+    public class EventHolder : MonoBehaviour
+    {
+        public UnityEvent someEvent;
+    }
+}
---
+++ Assets/Scrpits/SOEventSystem/EventListener.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Scrpits.SOEventSystem;
+using UnityEngine;
+
+public class EventListener : MonoBehaviour
+{
+    [SerializeField] private EventHolder A;
+
+    public void DoSomething()
+    {
+     
+    }
+}
